14edc68bc7f515f7f19d5d9ed3ed5627b2a6622b
lhd <lhd03@foxmail.com>
2023-08-07 16:32:11 +0800

    first commit

---
+++ src/main/java/com/lhd/GitTest.java
@@ -0,0 +1,11 @@
+package com.lhd;
+
+/**
+ * @author nine
+ * @date 2023/8/7 - 16:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
